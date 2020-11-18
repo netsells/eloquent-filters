1868b93baf87e6b85f9d9965027ff041c83cb982
adam <adam@pop-os.localdomain>
2020-11-18 12:07:04 +0000

    Fixes psr 4 autoloading

--- tests/Database/Migrations/0000_00_00_000000_create_test_model_table.php
+++ tests/Database/Migrations/0000_00_00_000000_create_test_model_table.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Netsells\EloquentFilters\Tests\Database\Migrations;
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
